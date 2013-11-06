02542a4a8531ffd02bdc762589c326ddef728cd7
Nathan Davies <hello@nathandavies.co.uk>
2013-11-06 17:55:50 +0000

    getUserMedia
    Integration of getUserMedia

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(function() {
-    $('body').addClass('js');
+    var errorMessage = $('#message .error');
+    
+    // Gather the user's media
+    navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia ||  navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+    
+    // Supported?
+    if (navigator.getUserMedia)
+        navigator.getUserMedia({ video: true, audio: true }, success, error);
+    else
+        errorMessage.html('Your browser is not supported');
+    
+    // We've got the user's media stream
+    function success() {
+        $('body').addClass('js');
+    }
+    
+    // Error occurred
+    function error() {
+        errorMessage.html('An error has occurred');
+    }
 });
