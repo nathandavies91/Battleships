95214d2c49581ed4ccdabb121fe2c8c75175a388
Nathan Davies <hello@nathandavies.co.uk>
2013-11-07 17:25:24 +0000

    Loading Indicator
    Few other improvements, and some sudo code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,55 @@
 /**
  * main.js
  * -------
- * Authors & Copyright (c) 2013: Nathan Davies, www.nathandavies.co.uk
+ * Author & Copyright (c) 2013: Nathan Davies, www.nathandavies.co.uk
  */
 
 jQuery(function($) {
     // Variables
     var message = $('#message');
     
-    // Initiate: Hide JavaScript error, and show start button
+    // Initiate: hide JavaScript error, and show the start button
     message.hide();
     $('<div id="start">Start</div>').insertAfter(message);
     
-    // Start Game
+    // Start the game initiation
     $('#start').bind('click', function() {
-        window.alert('Coming soon...');
+        // Loading indicator
+        Utilities.Loading.Start($(this), true);
+        
+        // Get the user media
+        //
+        
+        // Gather the user's media, hide the loading indicator
+        //
+        
+        // Peer
+        //
+        
+        // Show Game Board
     });
 });
+
+
+/**
+ * utilities.js
+ * ------------
+ * Author & Copyright (c) 2013: Nathan Davies, www.nathandavies.co.uk
+ */
+var Utilities = {
+    // Loading Indicator
+    Loading: {
+        loading: true,
+        Start: function(o, init) {
+            if (init || o.html().length >= 5) o.addClass('loading').html('.');
+            else o.append('.');
+            
+            // Call again in .5s
+            if (this.loading) setTimeout(function() { Utilities.Loading.Start(o); }, 500);
+            else this.loading = true;
+        },
+        Stop: function() {
+            this.loading = false;
+        }
+    }
+}
