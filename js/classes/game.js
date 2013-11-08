3f398a49ff3731d4b8c03d1444bd92f930101274
Nathan Davies <hello@nathandavies.co.uk>
2013-11-08 19:38:37 +0000

    Tidying

--- js/classes/game.js
+++ js/classes/game.js
@@ -7,21 +7,10 @@
 function Game() {
     Trace.Information('New Game()');
     
+    // Variables
+    var localGameBoard;
+    
     // Show the local player's game board
     $('h1').hide();
-    new GameBoard('local');
-    
-    // show peer sharing options; unless already in a party of 2
-    
-    /*
-        ability to be peer host
-        - don't allow more than one other in the party
-        - change peer sharing options with the start of other peer
-          - waiting for them to approve sharing of camera
-          - refused to share camera
-            - reenable sharing options
-            - reset score
-          - ready; let's play!
-            - draw remote board
-    */
+    localGameBoard = new GameBoard('local');
 }
