d6459b929d7a8212764fc1b982705ea7e9f48950
Nathan Davies <nathandavies@Nathans-MacBook-Pro.local>
2013-11-11 22:57:24 +0000

    Mute Local Video & Enhancements

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -26,19 +26,31 @@ function Game() {
 Game.prototype = {
     gridController: null,
     localMediaStream: null,
+    remoteMediaStream: null,
     
     // Show local game board
     LocalGameBoard: function() {
+        // Hide a few things
         $('h1').hide();
         $('#message').remove();
         
-        new GameBoard({id:'local'});
+        // Stream properties
+        var properties;
+        if (this.localMediaStream)
+            properties = {stream: this.localMediaStream.ObjectURL()};
+        
+        new GameBoard($.extend({id:'local',muted:true},properties));
         this.gridController.Resize();
     },
     
     // Show remote game board
     RemoteGameBoard: function() {
-        new GameBoard({id:'remote'});
+        // Stream properties
+        var properties;
+        if (this.remoteMediaStream)
+            properties = {stream: this.remoteMediaStream.ObjectURL()}
+        
+        new GameBoard($.extend({id:'remote'},properties));
         this.gridController.Resize();
     }
 }
--- js/html.js
+++ js/html.js
@@ -11,7 +11,7 @@ var HTML = {
         var template = '';
         template += '<section id="{{id}}" class="board">';
         template +=     '<aside>';
-        template +=         '<video{{#stream}} src="{{stream}}"{{/stream}} autoplay></video>';
+        template +=         '<video{{#stream}} src="{{stream}}"{{/stream}} autoplay{{#muted}} muted{{/muted}}></video>';
         template +=     '</aside>';
         template +=     '<section class="grid">';
         template +=         '{{#grid}}<div class="row">{{#.}}<div class="block"></div>{{/.}}</div>{{/grid}}';
